07eda36d2bf169902fb8c15755455f41689fe8dd
Sam Ivanov <si@juno-solar.com>
2023-11-05 17:40:59 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -51,83 +51,107 @@ namespace MiniPizzeria
             // Процес замовлення. Основний цикл
             while (addMoreItems)
             {
-                Console.Write("Please enter the code of the product you want to order: ");
-                if (!Enum.TryParse(Console.ReadLine(), out MenuCode code) || !Enum.IsDefined(typeof(MenuCode), code))
+                try
                 {
-                    Console.WriteLine("Invalid code, please try again.");
-                    continue;
+                    Console.Write("Please enter the code of the product you want to order: ");
+                    if (!Enum.TryParse(Console.ReadLine(), out MenuCode code) || !Enum.IsDefined(typeof(MenuCode), code))
+                    {
+                        Console.WriteLine("Invalid code, please try again.");
+                        continue;
+                    }
+
+                    Console.Write($"Enter the number of {code}s you want to order: ");
+                    if (!int.TryParse(Console.ReadLine(), out int quantity) || quantity <= 0)
+                    {
+                        Console.WriteLine("Invalid quantity, please enter a positive number.");
+                        continue;
+                    }
+
+                    decimal price = prices[code];
+                    // Додаткові дії, якщо замовлена піца.
+                    if (code <= MenuCode.PizzaHawaiian)
+                    {
+                        pizzaCount += quantity;
+                    }
+
+                    // Застосування знижки на напої при замовленні більше трьох.
+                    if (code > MenuCode.PizzaHawaiian && quantity > 3)
+                    {
+                        price *= 0.85m; // 15% знижка на напої
+                    }
+
+                    // Додавання товару до замовлення.
+                    if (order.ContainsKey(code))
+                    {
+                        order[code] += quantity;
+                    }
+                    else
+                    {
+                        order.Add(code, quantity);
+                    }
+
+                    // Проміжний розрахунок загальної суми для визначення кількості безкоштовних піц.
+                    totalAmount += price * quantity;
+
+                    Console.Write("Add more items to the order or print a receipt? (add/print): ");
+                    string choice = Console.ReadLine().ToLower();
+                    addMoreItems = choice == "add";
+
+                    if (choice == "print")
+                    {
+                        break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The format of the input is not valid. Please enter the correct format.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("The number is too large or too small. Please enter a valid number.");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
 
-                Console.Write($"Enter the number of {code}s you want to order: ");
-                if (!int.TryParse(Console.ReadLine(), out int quantity) || quantity <= 0)
-                {
-                    Console.WriteLine("Invalid quantity, please enter a positive number.");
-                    continue;
-                }
-
-                decimal price = prices[code];
-                // Додаткові дії, якщо замовлена піца.
-                if (code <= MenuCode.PizzaHawaiian)
-                {
-                    pizzaCount += quantity;
-                }
-
-                // Застосування знижки на напої при замовленні більше трьох.
-                if (code > MenuCode.PizzaHawaiian && quantity > 3)
-                {
-                    price *= 0.85m; // 15% знижка на напої
-                }
-
-                // Додавання товару до замовлення.
-                if (order.ContainsKey(code))
-                {
-                    order[code] += quantity;
-                }
-                else
-                {
-                    order.Add(code, quantity);
-                }
-
-                // Проміжний розрахунок загальної суми для визначення кількості безкоштовних піц.
-                totalAmount += price * quantity;
-
-                Console.Write("Add more items to the order or print a receipt? (add/print): ");
-                string choice = Console.ReadLine().ToLower();
-                addMoreItems = choice == "add";
-
-                if (choice == "print")
-                {
-                    break;
-                }
             }
 
-            // Застосування знижок.
-            // Знижка за безкоштовні піци.
-            int freePizzas = pizzaCount / 5;
-            decimal pizzaDiscount = freePizzas * prices[MenuCode.PizzaClassic];
-            totalAmount -= pizzaDiscount;
-
-            // Даємо знижка на все замовлення.
-            if (totalAmount > 50)
+            try
             {
-                totalAmount *= 0.8m; // 20% знижка на загальну суму замовлення, якщо вона більше $50.
-            }
 
-            // Вивод чеку.
-            Console.WriteLine("\nReceipt for payment:");
-            foreach (var item in order)
-            {
-                decimal itemTotalPrice = prices[item.Key] * item.Value;
-                // Коригування ціни, якщо є безкоштовні піци.
-                if (item.Key <= MenuCode.PizzaHawaiian && freePizzas > 0)
+                // Застосування знижок.
+                // Знижка за безкоштовні піци.
+                int freePizzas = pizzaCount / 5;
+                decimal pizzaDiscount = freePizzas * prices[MenuCode.PizzaClassic];
+                totalAmount -= pizzaDiscount;
+
+                // Даємо знижка на все замовлення.
+                if (totalAmount > 50)
                 {
-                    int deductablePizzas = Math.Min(freePizzas, item.Value);
-                    itemTotalPrice -= deductablePizzas * prices[MenuCode.PizzaClassic];
-                    freePizzas -= deductablePizzas; // Тут  віднімаємо кількість безкоштовних піц.
+                    totalAmount *= 0.8m; // 20% знижка на загальну суму замовлення, якщо вона більше $50.
                 }
-                Console.WriteLine($"{item.Key} - {item.Value} x ${prices[item.Key]} = ${itemTotalPrice}");
+
+                // Вивод чеку.
+                Console.WriteLine("\nReceipt for payment:");
+                foreach (var item in order)
+                {
+                    decimal itemTotalPrice = prices[item.Key] * item.Value;
+                    // Коригування ціни, якщо є безкоштовні піци.
+                    if (item.Key <= MenuCode.PizzaHawaiian && freePizzas > 0)
+                    {
+                        int deductablePizzas = Math.Min(freePizzas, item.Value);
+                        itemTotalPrice -= deductablePizzas * prices[MenuCode.PizzaClassic];
+                        freePizzas -= deductablePizzas; // Тут  віднімаємо кількість безкоштовних піц.
+                    }
+                    Console.WriteLine($"{item.Key} - {item.Value} x ${prices[item.Key]} = ${itemTotalPrice}");
+                }
+                Console.WriteLine($"Total to be paid: ${totalAmount:F2}");
             }
-            Console.WriteLine($"Total to be paid: ${totalAmount:F2}");
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred while calculating the total: {ex.Message}");
+            }
         }
     }
 }
